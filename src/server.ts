0f8ec8ec6121ae2265620aaf8a00f99ff34d5178
Md Junayed <devjunayed@gmail.com>
2024-10-11 01:09:56 +0600

    mongoose connectiondone

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,31 @@
+import mongoose from 'mongoose'
 import app from './app'
+import config from './app/config'
+import { Server } from 'http'
 
-app.listen(5000, () => {
-  console.log('server is running')
+let server: Server
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    server = app.listen(config.port, () => {
+      console.log(`Server is running at ${config.api_url}:${config.port} `)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+process.on('unhandledRejection', () => {
+  console.log('unhandled rejection is detected, shutting down')
+  if (server) {
+    server.close(() => {
+      process.exit(1)
+    })
+  }
 })
+
+process.on('uncaughtException', () => {
+  console.log('uncaughtExcepton is detected, shutting down')
+  process.exit(1)
+})
+main()
